8254264b230f38d9eb4f253a7b3267209e31c8b9
kumaresh varsha <126232332+kumareshvarsha@users.noreply.github.com>
2023-09-14 19:51:15 +0530

    Add files via upload

---
+++ CHAPTER-5/Loops.java
@@ -0,0 +1,119 @@
+
+public class Loops {
+    public static void main(String[] args) {
+
+        // while loop
+        // int i = 0;
+        // while (i<=3) {
+        //     System.out.println(i);
+        //     i++;            
+        // }
+
+
+
+        // infinite while loop
+        // while (true) {
+        //     System.out.println("I Love You Jesus");            
+        // }
+
+        // int i = 100;
+        // while (i<=200) {
+        // System.out.println(i);
+        // i++; 
+        // }
+
+
+
+        // do while loop
+        // int i=1;
+        // do {
+        //     System.out.println(i);
+        //     i++;
+        // } while (i<=10);
+
+        // int i=1;
+        // do {
+        //     System.out.println(i);
+        //     i++;
+        // } while (i<=100);
+
+
+
+        // for loop 
+        // for(int i=1; i<=10; i++)
+        // {
+        //     System.out.println(i);
+        // }
+        
+        // int n=5;//it means we want 5 odd numbers
+        // for(int i=0;i<n;i++){
+        //     System.out.println(2*i+1);
+        // }
+
+        // for(int i=10;i>0;i--){
+        //     System.out.println(i);
+        // }
+
+        // int n=20;
+        // for(int i=n; i>0;i--){
+        //     System.out.println(i);
+        // }
+
+
+
+        //break statement
+        // for(int i=1;i<10;i++){
+        //     System.out.println(i);
+        //     if(i==5){
+        //     System.out.println("Ending the execution");
+        //     break;
+        //     }
+        // }
+        // while loop
+        // int i=0;
+        // while (i<10) {
+        //     System.out.println(i);
+        //     if (i==5) {
+        //         System.out.println("Ending the execution");                
+        //         break;
+        //     }
+        //     i++;            
+        // }
+        // do while loop
+        // int i=0;
+        // do {
+        //     System.out.println(i);
+        //     i++;
+        //     if (i==5) {
+        //         System.out.println("Ending the loop");
+        //         break;
+        //     }
+        // } while (i<10);
+
+
+
+        // continue statement
+        // for loop
+        // for(int i=1;i<10;i++){
+        //     if (i==5) {
+        //         System.out.println("Dont execute the below code for this condition"); 
+        //         continue;               
+        //     }
+        //     System.out.println(i);
+        // }
+
+        // do while
+        // int i=0;
+        // do {
+        //     i++;
+        //     if (i==5) {
+        //         System.out.println("Dont execute the below code for this condition");
+        //         continue;
+        //     }
+        //     System.out.println(i);
+        // } while (i<10);
+               
+    }
+
+    
+}
---
+++ CHAPTER-5/PS5.java
@@ -0,0 +1,66 @@
+
+public class PS5 {
+    public static void main(String[] args) {
+        
+        // question 1
+        // for(int i=5;i>1;i--){
+        //     for(int j=1;j<i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.print("\n");
+        // }
+
+        // questio 2 first four even number sum
+        // int sum=0;
+        // int n=5;
+        // int i=1;
+        // while(i<=n){
+        //     sum=sum+2*i;            
+        //     i++;
+        // }
+        // System.out.println("sum is:"+sum);
+
+        // question 3
+        // int n=3;
+        // for(int i=1;i<=10;i++){
+        //     System.out.println("3*"+i+"="+n*i);
+        // }
+
+        // // question 4
+        // int n=10;
+        // for(int i=10;i>=1;i--){
+        //     System.out.println("10 x "+i+" = "+n*i);
+        // }
+
+        // question 5 and 6
+        // int n=5;
+        // int factorial = 1;
+        // int i=1;
+        // while(i<=n){
+        //     factorial = factorial*i;
+        //     i++;
+        // }
+        // System.out.println(factorial);
+
+        // question 7 already done 
+        // question 8th answer is true.we can do both loops 
+
+        // question 9
+        // int n=8;
+        // int sum = 0;
+        // for(int i=1;i<=10;i++){
+        //     System.out.println(n*i);
+        //     sum = sum + n*i;
+        // }
+        // System.out.println(sum);
+
+        // question 10
+        // we can execute the do-while loop atleast once.
+
+        // question 11
+        // already done.
+
+
+    }
+    
+}
