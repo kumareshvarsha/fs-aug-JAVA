b23d788563ee95a4aeeff2201e60ab688d617d03
kumaresh varsha <126232332+kumareshvarsha@users.noreply.github.com>
2023-09-14 19:50:51 +0530

    Add files via upload

---
+++ CHAPTER-4/ConditionS.java
@@ -0,0 +1,69 @@
+// import java.util.Scanner;
+public class ConditionS {
+    public static void main(String[] args) {
+
+        // if else Statement
+        // int a= 10;
+        // if (a>=18) {
+        //     System.out.println("You are a major.");            
+        // } else {
+        //     System.out.println("you are a minor.");            
+        // }
+
+        //Logical Operator in Conditional Statements
+        // 1.Using AND operator: the values should be true,if one of the value if false then it returns false.
+        // boolean a= true;
+        // boolean b= false;
+        // System.out.println(a && b);
+
+        // 2.Using OR operator:If one of the value is true it returns true.if both values or false then only it returns false.
+        // System.out.println(a || b);
+
+        // 3.Using NOT operator: It converts the values into opposite.if the value is true then it converts it into false,if the value is false then it converts it into true.
+        // System.out.println(!a);
+        // System.out.println(!b);
+
+
+        // if-else-if-else ladder 
+    //     System.out.println("Enter the age");
+    //     Scanner sc = new Scanner(System.in);
+    //     int age= sc.nextInt();
+    //     if(age>56){
+    //         System.out.println("you are a experienced person.");
+    //     }
+    //     else if(age>46){
+    //         System.out.println("you are a semi experienced person.");
+    //     }
+    //     else if(age>36){
+    //         System.out.println("you are a semi semi experienced person.");
+    //     }
+    //     else{
+    //         System.out.println("you are not a experienced person.");
+    //     }
+
+
+    // Switch case
+        // first method
+        // System.out.println("Enter the age");
+        // Scanner sc = new Scanner(System.in);
+        // int age= sc.nextInt();
+        //  second method
+        // int age = 21;
+        // switch (age){
+        //     case 18:
+        //     System.out.println("you are going to become an adult");
+        //     break;
+        //     case 21:
+        //     System.out.println("your are ready to join a job.");
+        //     break;
+        //     case 25:
+        //     System.out.println("your are ready for marriage.");
+        //     break;
+        //     case 60:
+        //     System.out.println("your are going to retiered.");
+        //     break;
+        //     default:
+        //    System.out.println("Enjoy the life");
+    //     }
+    }    
+}
---
+++ CHAPTER-4/PS4.java
@@ -0,0 +1,120 @@
+// import java.util.Scanner;
+
+public class PS4 {
+    public static void main(String[] args) {
+        // question 1
+        // int a =10;
+        // if (a=11){
+        //     System.out.println("I am 11");
+        // }
+        // else{
+        //     System.out.println("I am not 11");
+        // }
+        // it shows the error .because we cant assign(=) the value in if statement .we can use conditional or relational operator(==).
+
+        // question 2
+        // int s1,s2,s3;   
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter the Subject 1 marks:");
+        // s1 = sc.nextInt();
+        // System.out.print("Enter the subject 2 marks:");
+        // s2 = sc.nextInt();
+        // System.out.print("Enter the subject 3 marks:");
+        // s3 = sc.nextInt();
+        // int sum = s1+s2+s3;
+        // int total = 300;
+        // int percent = (sum*100)/total;
+        // System.out.println(percent+"%");
+        // if(percent>=40 && s1>33 && s2>33 && s3>33){
+        //     System.out.println("pass");
+        // }
+        // else{
+        //     System.out.println("false");
+        // }
+
+        // question 3
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter your income in lakhs per annum:");
+//         float tax = 0;
+//         float income = sc.nextFloat();
+//         if(income<=2.5f){
+//             tax = tax + 0;
+//         }
+//         else if(income > 2.5f && income<=5f){
+//             tax = tax + 0.05f * (income - 2.5f);
+//         }
+//         else if(income > 5f && income<=10f){
+//             tax = tax + 0.05f * (5f - 2.5f);
+//             tax = tax + 0.2f * (income - 5f);
+//         }
+//         else if(income > 10f){
+//             tax = tax + 0.05f * (5f - 2.5f);
+//             tax = tax + 0.2f * (10f - 5f);
+//             tax = tax + 0.3f * (income - 10f);
+//         }
+//         System.out.println("The total tax  paid by the employee is:"+tax);
+        
+
+        // question 4
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter the day:");
+        // int day = sc.nextInt();
+        // switch (day) {
+        //     case 1:
+        //     System.out.println("Its Monday");                
+        //     break;
+        //     case 2:
+        //     System.out.println("Its Tuesday");
+        //     break;
+        //     case 3:
+        //     System.out.println("Its Wednesday");
+        //     break;
+        //     case 4:
+        //     System.out.println("Its Thursday");
+        //     break;
+        //     case 5:
+        //     System.out.println("Friday");
+        //     break;
+        //     case 6:
+        //     System.out.println("Its saturday");
+        //     break;
+        //     case 7:
+        //     System.out.println("Its sunday");
+        //     break;      
+        //     default:
+        //     System.out.println("it will start a new week");
+        //         break;
+        // }
+
+
+        // question 5
+        // System.out.println("Enter anyone of the website:");
+        // Scanner sc = new Scanner(System.in);
+        // String web = sc.next();
+        // if(web.endsWith(".com")){
+        //     System.out.println("Its a Commercial Website");
+        // }
+        // else if(web.endsWith(".org")){
+        //     System.out.println("Its a Organisation Wesite");
+        // }
+        // else if (web.endsWith(".in")){
+        //     System.out.println("Its a Indian website");
+        // }
+        // else{
+        //     System.out.println("Its not a website");
+        // }
+
+        // question 6
+        //    System.out.println("Enter the year:");
+        //    Scanner sc = new Scanner(System.in);
+        //    int year = sc.nextInt();
+        //    if(year%4==0){
+        //     System.out.println("Its a leap year");
+        //    }
+        //    else{
+        //     System.out.println("Its not a leap year");
+        //    }
+
+
+    }    
+}
